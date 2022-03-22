7c0a5614cded62fceade4c951cc7b9bf29de8cd0
LizX <gjl9999@tutanota.com>
2022-03-21 17:33:45 -0700

    adding paragraphs continued

--- src/context/ResourceContext/resourceReducer.js
+++ src/context/ResourceContext/resourceReducer.js
@@ -9,7 +9,6 @@ export default (state, action) => {
                 let forHTML = [];
                 const regX = /\|/;
                 const newTextArray = article.split(regX);
-        
                     newTextArray.forEach((text, index) => {
                         if(index % 2 === 0) {
                             forHTML.push(text);
@@ -19,8 +18,43 @@ export default (state, action) => {
                             forHTML.push([[linkSplit[0], linkSplit[1]]]);
                         }
                     })
+                    // console.log(forHTML)
                 return forHTML;
             }
+            
+            // let splitRegion = [];
+            // const breakIntoNewParagraph = (input) => {
+            //     // let articleParagraph = [];
+            //     // let switchOver = 0;
+            //     // input.forEach(item=> {
+            //     //     articleParagraph[switchOver][].push(item);
+            //     //     if(item.includes('^')){
+            //     //         switchOver++;
+            //     //     }
+            //     // })
+            //     // return (articleParagraph)
+                
+
+            //     input.forEach((item, index) => {
+            //         if(item.includes("^")) {
+            //             splitRegion.push(index);
+            //         }
+            //     })
+            // }
+            let foo = parser(action.payload.article)
+            foo.toString();
+            console.log(foo)
+
+            // let sliced = []
+            // foo.forEach((item, index)=> {
+            //     let lastIndex = 0;
+            //     if(item.includes("^")) {
+            //         sliced.push(foo.slice(lastIndex, index));
+            //         lastIndex = index;
+            //         sliced.push(foo.slice(lastIndex, index));
+            //     }
+            //     console.log(sliced)
+            // })
             return {
                 ...state,
                 title: action.payload.title,
