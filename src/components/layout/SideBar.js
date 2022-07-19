50e8f1d42a2306853ad6a573aaa22a8faea60ce7
LizX <gjl9999@tutanota.com>
2022-07-19 15:18:20 -0700

    final cleaning done

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -3,7 +3,7 @@ const SideBar = () => {
     return (
         <aside className="medialinks">
         <nav id="medialinktext">
-            <h2 id="medialink"><i className="what-if-it-was-green">/</i>TeK_REsource 2021<i className="what-if-it-was-green">/</i></h2>
+            <h2 id="medialink"><i className="what-if-it-was-green">/</i>TeK_REsource 2022<i className="what-if-it-was-green">/</i></h2>
         </nav>
     </aside>
     )
